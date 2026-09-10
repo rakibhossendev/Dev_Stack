2a8ae308cccc871e2619ea3ff70760a3c7fc3040
rakibhossendev <rh01828315879@gmail.com>
2026-09-10 20:28:13 +0600

    Compelete navbar and added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 
 import './App.css'
+import {ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
   
 
   return (
     <>
-    
+
+    <Navbar></Navbar>
+    <ToastContainer/>
     </>
   )
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { IoMenu } from "react-icons/io5"
+import navLogo from "../../assets/logo-text.png"
+
+export default function Navbar(){
+
+    return (
+        <nav className="container mx-auto py-5 sticky">
+            <div className="flex justify-between">
+                <IoMenu className="block sm:hidden text-4xl cursor-pointer p-2 "></IoMenu>
+                
+                <img className="sm:mx-16" src={navLogo} alt="Navbar logo"/>
+
+                <ul className="hidden sm:flex justify-between gap-6">
+                    <li><a className="cursor-pointer  text-[#DB2777]" href="#">Home</a></li>
+                    <li><a className="cursor-pointer  hover:text-[#DB2777]" href="#">Technologies</a></li>
+                    <li><a className="cursor-pointer  hover:text-[#DB2777]" href="#">Projects</a></li>
+                    <li><a className="cursor-pointer  hover:text-[#DB2777]" href="#">About</a></li>
+                    <li><a className="cursor-pointer  hover:text-[#DB2777]" href="#">Contact</a></li>
+                </ul>
+
+                <div>
+                    <button className="mx-4 p-2 rounded-2xl text-[14px] cursor-pointer ">Sign In</button>
+                    <button className=" p-2 rounded-2xl text-[14px] bg-[#DB2777] text-white cursor-pointer hover:bg-[#e5aec7] hover:text-black">Sign Up</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
